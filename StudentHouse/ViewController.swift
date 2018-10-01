e3ab229cc9964666a13006535206252a79775ed9
Evan Weinberg <weinbergmath@emwcenter.local>
2018-10-01 21:14:47 +0700

    initial
    I believe I’ve added PromiseKit correctly, but I still can’t get a
    basic URLSession extension to work. I’m pretty sure I’m doing something
    wrong.

--- StudentHouse/ViewController.swift
+++ StudentHouse/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import PromiseKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var emailInput: UITextField!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var gradeLabel: UILabel!
+    
+    @IBOutlet weak var houseLabel: UILabel!
+    
+    @IBAction func searchEmail(_ sender: Any) {
+        
+   
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
+        
+        
+        
+        let url = URL(string:"www.cnn.com")!
+        
+        let promise = URLSession.shared.dataTask(.promise, with: url)
+        
+    
+    
+    
+}
 }
 
